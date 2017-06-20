26de69944eaecbdd816b7887f882cb930596387b
Simon Towler <simontowler@Simons-MacBook-Air.local>
2017-06-20 19:00:14 +0100

    Stubbed out an initial unit test.

---
+++ src/test/java/CcNumsTests.java
@@ -0,0 +1,24 @@
+package ccnums;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Simon
+ */
+public class CcNumsTests {
+    
+    @Test
+    public void testCcNums() {
+        String testNumber = "376039019752033";//australia card
+    
+        CcNums testObject = new CcNums();
+        
+        //CcNumber number = testObject.getCcNumber(testNumber);
+        
+        assertTrue(testObject != null);
+
+    }
+
+}
