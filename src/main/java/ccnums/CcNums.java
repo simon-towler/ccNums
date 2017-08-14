2eda218dc2591ca100db01b5686557b9c55ed5e3
simon-towler <simon.towler@icloud.com>
2017-08-14 19:15:39 +0100

    Added filtering of non-numerics from the input number string.

--- src/main/java/ccnums/CcNums.java
+++ src/main/java/ccnums/CcNums.java
@@ -15,9 +15,16 @@ public class CcNums {
     }
 
     public static CcNumber getCcNumber(String number) {
+        number = filterNonNumerics(number);
         if (numberIsValid(number)) return CcNumber.createNumber(number);
         //TODO what if the number is not valid? how to tell the user
         //TODO don't return null, do something else
         return null;
+    }
+
+    private static String filterNonNumerics(String number) {
+        // modelled on https://stackoverflow.com/questions/1533659/how-do-i-remove-the-non-numeric-character-from-a-string-in-java
+        // accessed 14aug17
+        return number.replaceAll( "[^\\d]", "" );
     }
 }
