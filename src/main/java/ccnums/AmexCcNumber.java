8ecdcb2124eae0c312ef0aede28adf8e54ce044c
simon-towler <simon.towler@icloud.com>
2017-08-21 18:57:09 +0100

    Removed unnecessary overrides from AmexCcNumber.

--- src/main/java/ccnums/AmexCcNumber.java
+++ src/main/java/ccnums/AmexCcNumber.java
@@ -4,35 +4,12 @@ package ccnums;
  * Created by simontowler on 10/07/2017.
  */
 class AmexCcNumber extends ccnums.CcNumber {
-    // variables
-    static final String issuer = "AmericanExpress";//TODO make constants for issuer strings
-
-    // instance variables
-    String grouping = groupingPatterns.getProperty(getIssuer());
 
     // constructor
     AmexCcNumber(String number) {
         setNumber(number);
-    }
-
-    @Override
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-
-    public String getIssuer() {
-            return issuer;
-        }
-
-    @Override
-    public String getNumber() {
-        return(number);
-    }
-
-    @Override
-    public String getNumberGrouped() {
-        return number.replaceAll(grouping, "$1 $2 $3");
+        setIssuer("AmericanExpress");
+        setGrouping(getNumber());
     }
 
 }
