07306f69fcd442e49213897102e79796c5ed771d
simon-towler <simon.towler@icloud.com>
2017-08-31 18:00:11 +0100

    Removed an earlier test because a later one makes it redundant.

--- src/test/java/CcNumsTests.java
+++ src/test/java/CcNumsTests.java
@@ -13,7 +13,9 @@ import static org.junit.Assert.*;
  * @author Simon
  */
 public class CcNumsTests {
-
+    /*
+    *  Also tests masking and grouping.
+    */
     @Test
     public void testTruncate() throws Exception {
 
@@ -133,56 +135,6 @@ public class CcNumsTests {
         System.out.println("Visa 13 masked to PCI DSS is: " + maskResponse);
         assertTrue(maskResponse.equals("4222 22* **2 222"));
         System.out.println();
-    }
-    
-    @Test
-    public void testCcNums() {
-
-        String amexTestNumber = "376039019752033";//amex australia card
-        // Test numbers from https://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm accessed 17aug17
-        String visaTestNumber01 = "4111111111111111";
-        String visaTestNumber02 = "4012888888881881";
-        String visaTestNumber03 = "4222222222222";
-        
-        CcNumber amexNumber01 = CcNums.getCcNumber(amexTestNumber);
-
-        assertTrue(amexNumber01.getIssuer().equals("AmericanExpress"));
-
-        assertTrue(amexNumber01 != null);
-        //TODO remove
-            System.out.println("The Amex credit card number is: " + amexNumber01.getNumber());
-        assertTrue(amexNumber01.getNumber().equals(amexTestNumber));
-        //TODO remove
-            //System.out.println(number.getGroupingPattern());
-            System.out.println("That Amex number when grouped is: " + amexNumber01.getNumberGrouped());
-        assertTrue(amexNumber01.getNumberGrouped().equals("3760 390197 52033"));
-
-
-        //VISA 16 NUMBER TEST
-        CcNumber visaNumber02 = CcNums.getCcNumber(visaTestNumber02);
-        assertTrue(visaNumber02 != null);
-        assertTrue(visaNumber02.getIssuer().equals("Visa"));
-
-
-        //TODO remove
-        System.out.println("When the Visa credit card number is: " + visaNumber02.getNumber());
-        assertTrue(visaNumber02.getNumber().equals(visaTestNumber02));
-        //TODO remove
-        System.out.println("That Visa number when grouped is: " + visaNumber02.getNumberGrouped());
-        assertTrue(visaNumber02.getNumberGrouped().equals("4012 8888 8888 1881"));
-
-        //VISA 13 NUMBER TEST
-        CcNumber visaNumber03 = CcNums.getCcNumber(visaTestNumber03);
-        assertTrue(visaNumber03 != null);
-        assertTrue(visaNumber03.getIssuer().equals("Visa"));
-
-
-        //TODO remove
-        System.out.println("And when the Visa credit card number is: " + visaNumber03.getNumber());
-        assertTrue(visaNumber03.getNumber().equals(visaTestNumber03));
-        //TODO remove
-        System.out.println("That Visa number when grouped is: " + visaNumber03.getNumberGrouped());
-        assertTrue(visaNumber03.getNumberGrouped().equals("4222 222 222 222"));
     }
 
     @Test
